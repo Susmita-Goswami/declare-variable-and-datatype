c95fd78239666ef79adb0c7aada34ad9cd248b2e
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-24 18:06:33 +0600

    Add files via upload

---
+++ datatype1.c
@@ -0,0 +1,12 @@
+
+/*An interger variable number
+that will contain 10,
+Write a program that prints an integer number*/
+#include<stdio.h>
+int main(void)
+{
+    int num1=10,num2=20;
+
+    printf("number is %d\n",num1);
+    printf("number is %d",num2);
+}
